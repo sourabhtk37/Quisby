8d43cd489aa166a927ca624ae1e65c209d4d8ea4
T K Sourab <tsourab@redhat.com>
2020-09-19 02:43:10 +0530

    Add linpack comparison and extract modules
    Signed-off-by: T K Sourab <tsourab@redhat.com>

---
+++ linpack/extract.py
@@ -0,0 +1,32 @@
+import csv
+
+import cloud_pricing
+from config import cloud_type, region
+
+
+def extract_linpack_summary_data(path, system_name):
+    """
+    Make shift function to handle linpack summary data 
+    till a resolution is reached
+    """
+
+    results = []
+
+    with open(path) as csv_file:
+        csv_reader = csv.DictReader(csv_file, delimiter=':')
+
+        for row in csv_reader:
+            gflops = row['MB/sec']
+
+    if gflops:
+        get_cloud_pricing = getattr(
+            cloud_pricing, 'get_%s_pricing' % cloud_type.lower())
+        price_per_hour = get_cloud_pricing(system_name, region)
+
+        results.append(system_name)
+        results.append(gflops)
+        results.append(1)
+        results.append(price_per_hour)
+        results.append(float(gflops)/float(price_per_hour))
+
+        return [results]
