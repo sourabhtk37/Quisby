c2fe9b9ab38f014068266fa2575656424d30b64c
sousinha <sousinha@redhat.com>
2022-05-18 14:05:54 +0530

    missed code

--- quisby/config.py
+++ quisby/config.py
@@ -5,17 +5,17 @@
 # spreadsheetId = '1aUwUL99-FHfH0NdbxfGW886sqSEXVU2yVBOXAJf2RXc'
 # Leave empty if you want a new spreadsheet to be created.
 # TODO: user name and time
-spreadsheetId = ""
-OS_TYPE = ""
-OS_RELEASE = ""
-spreadsheet_name = ""
+spreadsheetId = "1L6hkm3S-RLq0JeCvxZnrpuI8WfVdgnlOnQJ4tM6vCb0"
+OS_TYPE = "rhel"
+OS_RELEASE = "9"
+spreadsheet_name = "ug"
 
 test_name = ""
-test_path = ""
+test_path = "/Users/soumyasinha/Workspace/4_may_data_rhel9/"
 
 system_name = ""
-region = "US East 2"
-cloud_type = ""
+region = "us-east-1"
+cloud_type = "aws"
 
 specjbb_java_version = "java-11-openjdk"
 # ================================
