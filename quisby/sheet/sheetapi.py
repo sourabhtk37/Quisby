7c5751584874b8dd9d2031b5b2320a3ef146f949
sousinha97 <sousinha@redhat.com>
2023-01-18 19:04:11 +0530

    add discovery url

--- quisby/sheet/sheetapi.py
+++ quisby/sheet/sheetapi.py
@@ -3,7 +3,7 @@ from google.oauth2 import service_account
 
 SCOPES=['https://www.googleapis.com/auth/spreadsheets','https://www.googleapis.com/auth/spreadsheets.readonly','https://www.googleapis.com/auth/drive']
 SERVICE_ACCOUNT_FILE = 'credentials.json'
-
+DISCOVERY_SERVICE_URL = 'https://sheets.googleapis.com/$discovery/rest?version=v4'
 creds = service_account.Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes=SCOPES)
-service = build("sheets", "v4", credentials=creds)
+service = build("sheets", "v4", credentials=creds,discoveryServiceUrl=DISCOVERY_SERVICE_URL)
 sheet = service.spreadsheets()
