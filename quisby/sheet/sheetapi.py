a9718ef4f974c9c216bfbbd10c70b3c6956c03ac
sousinha97 <sousinha@redhat.com>
2023-03-09 16:17:59 +0530

    use env variable to fetch home directory path

--- quisby/sheet/sheetapi.py
+++ quisby/sheet/sheetapi.py
@@ -1,8 +1,10 @@
 from googleapiclient.discovery import build
 from google.oauth2 import service_account
+import os
 
+home_dir = os.getenv("HOME")
 SCOPES=['https://www.googleapis.com/auth/spreadsheets','https://www.googleapis.com/auth/spreadsheets.readonly','https://www.googleapis.com/auth/drive']
-SERVICE_ACCOUNT_FILE = '~/.config/quisby/credentials.json'
+SERVICE_ACCOUNT_FILE = home_dir+'/.config/quisby/credentials.json'
 DISCOVERY_SERVICE_URL = 'https://sheets.googleapis.com/$discovery/rest?version=v4'
 creds = service_account.Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes=SCOPES)
 service = build("sheets", "v4", credentials=creds,discoveryServiceUrl=DISCOVERY_SERVICE_URL)
