000b14bef85450ae27cf93d66dd00c81ed058eff
sousinha97 <sousinha@redhat.com>
2023-07-10 12:42:38 +0530

    Pyperf added

---
+++ quisby/benchmarks/pyperf/graph.py
@@ -0,0 +1,124 @@
+
+from quisby.sheet.sheet_util import read_sheet,clear_sheet_charts,get_sheet
+from quisby.sheet.sheetapi import sheet
+import time
+
+def create_series_range_list_pyperf(column_count, sheetId, start_index, end_index):
+    series = []
+
+    for index in range(column_count):
+
+        series.append(
+            {
+                "series": {
+                    "sourceRange": {
+                        "sources": [
+                            {
+                                "sheetId": sheetId,
+                                "startRowIndex": start_index,
+                                "endRowIndex": end_index,
+                                "startColumnIndex": index + 1,
+                                "endColumnIndex": index + 2,
+                            }
+                        ]
+                    }
+                },
+                "type": "COLUMN",
+            }
+        )
+
+    return series
+
+def graph_pyperf_data(spreadsheetId,range):
+    GRAPH_COL_INDEX = 3
+    GRAPH_ROW_INDEX = 10
+    start_index = 0
+    end_index = 0
+
+    data = read_sheet(spreadsheetId, range)
+    clear_sheet_charts(spreadsheetId, range)
+
+    for index, row in enumerate(data):
+        if "GEOMEAN" in row:
+            start_index = index
+        a = len(data)
+        if start_index:
+            if row == []:
+                end_index = index - 1
+            if index + 1 == len(data):
+                end_index = index + 1
+
+        if end_index:
+
+            graph_data = data[start_index:end_index]
+            column_count = len(graph_data[0])
+
+            sheetId = get_sheet(spreadsheetId, range)["sheets"][0]["properties"][
+                "sheetId"
+            ]
+
+            requests = {
+                "addChart": {
+                    "chart": {
+                        "spec": {
+                            "title": "%s : %s" % (range, "GEOMEAN"),
+                            "basicChart": {
+                                "chartType": "COLUMN",
+                                "legendPosition": "BOTTOM_LEGEND",
+                                "axis": [
+                                    {"position": "BOTTOM_AXIS", "title": ""},
+                                    {
+                                        "position": "LEFT_AXIS",
+                                        "title": "geomean",
+                                    },
+                                ],
+                                "domains": [
+                                    {
+                                        "domain": {
+                                            "sourceRange": {
+                                                "sources": [
+                                                    {
+                                                        "sheetId": sheetId,
+                                                        "startRowIndex": start_index,
+                                                        "endRowIndex": end_index,
+                                                        "startColumnIndex": 0,
+                                                        "endColumnIndex": 1,
+                                                    }
+                                                ]
+                                            }
+                                        }
+                                    }
+                                ],
+                                "series": create_series_range_list_pyperf(
+                                    column_count, sheetId, start_index, end_index
+                                ),
+                                "headerCount": 1,
+                            },
+                        },
+                        "position": {
+                            "overlayPosition": {
+                                "anchorCell": {
+                                    "sheetId": sheetId,
+                                    "rowIndex": GRAPH_ROW_INDEX,
+                                    "columnIndex": column_count + GRAPH_COL_INDEX,
+                                }
+                            }
+                        },
+                    }
+                }
+            }
+
+            if GRAPH_COL_INDEX >= 5:
+                GRAPH_ROW_INDEX += 20
+                GRAPH_COL_INDEX = 1
+            else:
+                GRAPH_COL_INDEX += 6
+
+            body = {"requests": requests}
+
+            sheet.batchUpdate(spreadsheetId=spreadsheetId, body=body).execute()
+
+            # Reset variables
+            start_index, end_index = 0, 0
+
+            time.sleep(1)
