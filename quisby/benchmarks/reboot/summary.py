0b8ce7a1da1af0215ec4b722e0d093ad13bb81f0
sousinha97 <sousinha@redhat.com>
2023-09-25 15:19:06 +0530

    missed

--- quisby/benchmarks/reboot/summary.py
+++ quisby/benchmarks/reboot/summary.py
@@ -1,7 +1,9 @@
 from itertools import groupby
 
+from quisby.util import mk_int, process_instance
 
-def create_summary_boot_data(results):
+
+def create_summary_boot_data(results,os_release):
     sorted_results = []
 
     results = list(filter(None, results))
@@ -9,10 +11,10 @@ def create_summary_boot_data(results):
     results = [row for row in results if row[0] != "System name"]
 
     for _, items in groupby(sorted(results), key=lambda x: x[0].split(".")[0]):
-
-        sorted_data = sorted(
-            list(items), key=lambda x: int(x[0].split(".")[1].split("x")[0])
-        )
+        sorted_data= sorted(list(items), key=lambda x: mk_int(process_instance(x[0], "size")))
+        # sorted_results += sorted(
+        #     list(items), key=lambda x: int(x[0].split(".")[1].split("x")[0])
+        # )
 
         sorted_results += header_row + sorted_data
 
